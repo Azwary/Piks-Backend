3e969538430d7218a2fad701db846130fb63cf96
veroagrisand <agrisandavero@gmail.com>
2024-12-13 15:41:31 +0700

    corrrrrss

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-
+ 
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
@@ -19,16 +19,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
-
+    'allowed_origins' => ['*'],
+    
     'allowed_origins_patterns' => [],
-
+    
     'allowed_headers' => ['*'],
-
+    
     'exposed_headers' => [],
-
+    
     'max_age' => 0,
-
+    
     'supports_credentials' => false,
 
 ];
