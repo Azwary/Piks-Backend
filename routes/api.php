f54163308620d86f6920ff20acdddd7ef570fa56
Azwary <azwrysf23@gmail.com>
2024-12-15 05:36:59 +0700

    05.36

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware(['auth:sanctum', 'role:1'])->group(function () {      //pengel
     Route::get('/total-pending', [AduanController::class, 'total1']);
     Route::get('/pending', [AduanController::class, 'pending']);
     Route::put('/updateStatus/{id}', [AduanController::class, 'updateStatus']);
+    Route::put('/toproses/{id}', [AduanController::class, 'toproses']);
     Route::delete('aduan/{id}', [AduanController::class, 'destroy']);
 });
 
