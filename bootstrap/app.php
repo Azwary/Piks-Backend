3b23b18d6d10fd33d6eb80f47407066ebb33896d
veroagrisand <agrisandavero@gmail.com>
2024-12-14 14:18:35 +0700

    g

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-<<<<<<< HEAD
-        //
-=======
->>>>>>> fb/main
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
