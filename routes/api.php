a8b2da44075dce46e76b689df467a7357dc1e390
veroagrisand <agrisandavero@gmail.com>
2024-12-14 15:39:42 +0700

    aduan

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\kategoriController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\AduanController;
 use App\Http\Controllers\AuthController;
@@ -8,27 +8,32 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//Auth
+// Auth routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Authenticated routes
 Route::middleware(['auth:sanctum'])->group(function () {
+    // Fetch authenticated user details
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
     // Add logout route
     Route::post('/logout', [AuthController::class, 'logout']);
 });
-// kategori
-Route::get('/kategori', [kategoriController::class, 'index']);
-Route::get('/kategori/{id}', [kategoriController::class, 'show']);
-// Setatus
+
+// Kategori routes
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/kategori/{id}', [KategoriController::class, 'show']);
+
+// Status routes
 Route::get('/status', [StatusController::class, 'index']);
 Route::get('/status/{id}', [StatusController::class, 'show']);
-// Aduan
-Route::get('/Aduan', [AduanController::class, 'index'])->name('Aduan.index');
-Route::get('/Aduan/{id}', [AduanController::class, 'show'])->name('Aduan.show');
-Route::post('/Aduan', [AduanController::class, 'store'])->name('Aduan.store');
-Route::put('/Aduan/{id}', [AduanController::class, 'update'])->name('Aduan.update');
-Route::delete('/Aduan/{id}', [AduanController::class, 'destroy'])->name('Aduan.destroy');
 
+// Aduan routes
+Route::get('/aduan', [AduanController::class, 'index'])->name('aduan.index');
+Route::get('/aduan/{id}', [AduanController::class, 'show'])->name('aduan.show');
+Route::post('/aduan', [AduanController::class, 'store'])->name('aduan.store');
+Route::put('/aduan/{id}', [AduanController::class, 'update'])->name('aduan.update');
+Route::delete('/aduan/{id}', [AduanController::class, 'destroy'])->name('aduan.destroy');
